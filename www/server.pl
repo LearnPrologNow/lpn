553aa22d49e23bf60dc62fc8cc432be1559bdb61
Paul Brown <pbrownpaul@gmail.com>
2019-08-26 00:00:14 +0100

    Added Production server daemon

--- www/server.pl
+++ www/server.pl
@@ -3,6 +3,10 @@
 
 :- ensure_loaded(handlers).
 
+go :-
+    use_module(library(http/http_unix_daemon)),
+    initialization(http_daemon).
+
 server(Port) :-
     http_server(http_dispatch, [port(Port)]).
 
