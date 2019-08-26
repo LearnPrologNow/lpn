5d762bf3b76fac32e041fc1ead34fa9e026fa69d
Nicolas Gallinal <ngallinal@hexacta.com>
2019-08-25 22:26:41 -0300

    use $.get instead of old XmlHttpRequest object

--- www/static/js/lpn.js
+++ www/static/js/lpn.js
@@ -65,19 +65,14 @@ function open_parents(id) {
 
 // loadPl used in quizes to consult prolog
 function loadPl(session, location) {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-         const parsed = session.consult(this.responseText);
+    $.get(location, function(data) {
+        const parsed = session.consult(data);
          if (parsed !== true) { console.log(pl.format_answer(parsed)) }
         // Run query, can only load one file this way
          const query = session.query("init.")
          if (query !== true) { console.log(pl.format_answer(query)) }
          session.answer((a) => {}) //console.log(pl.format_answer(a)))
-    }
-  };
-  xhttp.open("GET", location, true);
-  xhttp.send();
+    });
 }
 
 let session = null
